bf89a88c0c6a189048209aff3ec38852934d850c
ynKim-dev <82657096+ynKim-dev@users.noreply.github.com>
2021-06-07 12:40:32 +0900

    Update Practice6.java
    문제 내용 잘못 이해로 변경 후 커밋

--- practice.kunsan.ac/src/practice/kunsan/ac/Practice6.java
+++ practice.kunsan.ac/src/practice/kunsan/ac/Practice6.java
@@ -7,17 +7,36 @@ public class Practice6 {
 
 	public static void main(String[] args) {
 
-		Scanner scan = new Scanner(System.in);
-
 		float[] arr = new float[10];
+		String text = "90,75,85,95,70,75,85,85,95,72";
+		char check_Text;
+		int count = 0, number = 0, count_Number = 0;
+		System.out.println("점수: " + text);
+		for (int i = text.length() - 1; i >= 0; i--) {
 
-		System.out.print("점수: ");
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = scan.nextFloat();
+			check_Text = text.charAt(i);
+
+			if (check_Text == ',') {
+				arr[count] = (float) number;
+				count++;
+				count_Number = 0;
+				number = 0;
+			} else {
+				number = number + ((int) check_Text - 48) * (int) Math.pow(10, count_Number);
+				count_Number++;
+				if (i == 0) {
+					arr[count] = (float) number;
+				}
+			}
+			System.out.println(check_Text + " 변환  " + number);
+
 		}
 
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
 		System.out.println("합계: " + get_Sum(arr) + "\n평균: " + get_Sum(arr) / 10);
-		scan.close();
+
 	}
 
 	public static float get_Sum(float[] arr) {
@@ -28,5 +47,4 @@ public class Practice6 {
 
 		return sum;
 	}
-
 }
