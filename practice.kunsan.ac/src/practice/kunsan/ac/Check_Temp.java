6a070f3b509dd1a90d403a9aa48c350e320bd5c2
ynKim-dev <82657096+ynKim-dev@users.noreply.github.com>
2021-06-09 10:20:26 +0900

    Update Check_Temp.java

--- practice.kunsan.ac/src/practice/kunsan/ac/Check_Temp.java
+++ practice.kunsan.ac/src/practice/kunsan/ac/Check_Temp.java
@@ -2,22 +2,21 @@ package practice.kunsan.ac;
 
 import java.util.*;
 
-// 섭씨 온도를 화씨 온도로 변환하는 프로젝트
-public class Check_Temp {
+public class Main {
 
 	public static void main(String[] args) {
 
-		check_Temperature();
+		Scanner scan = new Scanner(System.in);
+		double temperature = 0;
+		System.out.print("섭씨: ");
+		temperature = scan.nextFloat();
+		System.out.println("화씨: " + change_Celsius_To_Fahrenheit(temperature));
+		scan.close();
 	}
 
-	public static void check_Temperature() {
-		Scanner scan = new Scanner(System.in);
-		double temp = 0, temp1 = 0;
-		System.out.print("섭씨: ");
-		temp = scan.nextFloat();
-		temp1 = (temp * 1.8) + 32;
-		System.out.println("화씨: " + temp1);
-		scan.close();
+	public static double change_Celsius_To_Fahrenheit(double temperature) {
+
+		return (temperature * 1.8) + 32;
 	}
 
 }
