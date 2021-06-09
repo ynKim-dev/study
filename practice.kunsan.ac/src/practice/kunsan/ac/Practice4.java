a7ccf230530bb64382d9948e25b79675313e63e0
ynKim-dev <82657096+ynKim-dev@users.noreply.github.com>
2021-06-09 10:25:04 +0900

    Update Practice4.java

--- practice.kunsan.ac/src/practice/kunsan/ac/Practice4.java
+++ practice.kunsan.ac/src/practice/kunsan/ac/Practice4.java
@@ -12,12 +12,12 @@ public class Practice4 {
 	public static void create_Star_Triangle() {
 		Scanner scan = new Scanner(System.in);
 
-		int temp = 0;
+		int number = 0;
 
 		System.out.print("Enter line number: ");
-		temp = scan.nextInt();
+		number = scan.nextInt();
 
-		for (int i = 1; i <= temp; i++) {
+		for (int i = 1; i <= number; i++) {
 			for (int j = 1; j <= i; j++) {
 				System.out.print("*");
 			}
