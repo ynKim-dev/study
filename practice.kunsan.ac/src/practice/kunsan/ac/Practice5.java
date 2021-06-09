90acdbe55c5f75cafea5da60811b056a72d519b5
ynKim-dev <82657096+ynKim-dev@users.noreply.github.com>
2021-06-09 10:50:47 +0900

    Update Practice5.java

--- practice.kunsan.ac/src/practice/kunsan/ac/Practice5.java
+++ practice.kunsan.ac/src/practice/kunsan/ac/Practice5.java
@@ -10,12 +10,9 @@ public class Practice5 {
 	}
 
 	public static void calculate_Grade() {
-		Scanner scan = new Scanner(System.in);
 
-		int number = 0;
+		int number = check_Number();
 		String grade = "";
-		System.out.print("점수 입력: ");
-		number = scan.nextInt();
 
 		if (number >= 90)
 			grade = "A";
@@ -29,7 +26,33 @@ public class Practice5 {
 			grade = "F";
 
 		System.out.println("학점: " + grade);
+
+	}
+
+	// 점수의 이상 값들을 감지해서 다시 입력받게 하는 함수
+	public static int check_Number() {
+		Scanner scan = new Scanner(System.in);
+		int input_number = 0;
+		while (true) {
+			try {
+				System.out.print("점수 입력: ");
+				input_number = scan.nextInt();
+				if (input_number >= 0 && input_number <= 100)
+					break;
+				else
+					throw new Exception();
+
+			} catch (InputMismatchException ime) {
+				System.out.println("정수 값만 입력하세요!!");
+				scan.next();
+				continue;
+			} catch (Exception e) {
+				System.out.println("0부터 100 사이 값만 입력하세요!!");
+				continue;
+			}
+		}
 		scan.close();
+		return input_number;
 	}
 
 }
