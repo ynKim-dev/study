2d217d94e0d4ee35643c39b535b60b0a908042f7
ynKim-dev <82657096+ynKim-dev@users.noreply.github.com>
2021-06-14 12:34:15 +0900

    Update Practice7.java

--- practice.kunsan.ac/src/practice/kunsan/ac/Practice7.java
+++ practice.kunsan.ac/src/practice/kunsan/ac/Practice7.java
@@ -5,10 +5,10 @@ public class Practice7 {
 
 	public static void main(String[] args) {
 
-		calcScore();
+		calculateScore();
 	}
 
-	public static void calcScore() {
+	public static void calculateScore() {
 		int[][] arr = { { 90, 80, 80 }, { 85, 85, 95 }, { 95, 70, 75 }, { 80, 80, 90 }, { 90, 75, 80 } };
 		int sum = 0;
 
