65ac41b8c98edab7ddfd3cb9355760cd6ddad4d5
ynKim-dev <82657096+ynKim-dev@users.noreply.github.com>
2021-06-14 12:05:40 +0900

    Update Practice9.java

--- practice.kunsan.ac/src/practice/kunsan/ac/Practice9.java
+++ practice.kunsan.ac/src/practice/kunsan/ac/Practice9.java
@@ -3,21 +3,46 @@ package practice.kunsan.ac;
 // 시저의 암호기법을 적용하는 프로젝트
 public class Practice9 {
 
+	private static Scanner inputNumber = new Scanner(System.in);
+	private static Scanner inputText = new Scanner(System.in);
+	private static Scanner inputCipher = new Scanner(System.in);
+
 	public static void main(String[] args) {
 
-		change_To_Caesar_cipher();
+		printMenu();
 	}
 
-	public static void change_To_Caesar_cipher() {
-		String text = "Hello World";
-		String answer = "";
+	public static void printMenu() {
 		int number = 0;
-		char temp = 0;
+		while (true) {
+			System.out.println("1.암호화\n2.복호화\n3.종료");
+			number = inputNumber.nextInt();
+			if (number == 1)
+				encryptCipher();
+			else if (number == 2)
+				decryptCipher();
+			else if (number == 3)
+				break;
+			else
+				continue;
+		}
+		inputNumber.close();
+		inputText.close();
+		inputCipher.close();
+	}
+
+	public static void encryptCipher() {
+		String text, answer = "";
+		int number = 0;
+		char letter = 0;
+
+		System.out.println("암호화할 문장을 입력하세요!");
+		text = inputText.nextLine();
 
 		for (int i = 0; i < text.length(); i++) {
-			temp = text.charAt(i);
+			letter = text.charAt(i);
 
-			number = (int) temp;
+			number = (int) letter;
 
 			if ((number >= 97 && number <= 119) || (number >= 65 && number <= 87))
 				number = number + 3;
@@ -27,6 +52,31 @@ public class Practice9 {
 			answer = answer + (char) number;
 
 		}
-		System.out.println(text + "\n" + answer);
+		System.out.println("평문: " + text + "\n" + "암호문: " + answer);
 	}
+
+	public static void decryptCipher() {
+		String text, answer = "";
+		int number = 0;
+		char letter = 0;
+
+		System.out.println("복호화할 문장을 입력하세요!");
+		text = inputCipher.nextLine();
+
+		for (int i = 0; i < text.length(); i++) {
+			letter = text.charAt(i);
+
+			number = (int) letter;
+
+			if ((number >= 97 && number <= 99) || (number >= 65 && number <= 67))
+				number = number + 29;
+			else if ((number >= 100 && number <= 122) || (number >= 68 && number <= 90))
+				number = number - 3;
+
+			answer = answer + (char) number;
+
+		}
+		System.out.println("암호문: " + text + "\n" + "평문: " + answer);
+	}
+
 }
