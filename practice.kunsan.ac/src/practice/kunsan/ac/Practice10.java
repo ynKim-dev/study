ed5f3e205c8b57d88c65ca40ffa0535da10cfade
ynKim-dev <82657096+ynKim-dev@users.noreply.github.com>
2021-06-15 11:48:53 +0900

    Update Practice10.java

--- practice.kunsan.ac/src/practice/kunsan/ac/Practice10.java
+++ practice.kunsan.ac/src/practice/kunsan/ac/Practice10.java
@@ -1,47 +1,16 @@
 package practice.kunsan.ac;
 
-import java.util.*;
+
 
 // 사칙연산을 함수를 통해 계산하는 프로젝트
 public class Practice10 {
 
 	public static void main(String[] args) {
 
-		Scanner scan = new Scanner(System.in);
-		float number1 = 0, number2 = 0, count = 0;
+		float result = 0;
+		result = subtract(divide(multiply(subtract(add(3, 5), 2), 2), 3), 4);
 
-		System.out.print("숫자 입력(처음 입력시, 2개 입력) : ");
-		while (true) {
-			if (count == 0)
-				number1 = scan.nextFloat();
-
-			number2 = scan.nextFloat();
-
-			if (count == 0) {
-				System.out.println(number1 + " + " + number2);
-				number1 = add(number1, number2);
-
-			} else if (count == 1 || count == 4) {
-				System.out.println(number1 + " - " + number2);
-				number1 = subtract(number1, number2);
-			} else if (count == 2) {
-				System.out.println(number1 + " X " + number2);
-				number1 = multiply(number1, number2);
-			} else if (count == 3) {
-				System.out.println(number1 + " / " + number2);
-
-				if (number2 == 0) {
-					System.out.println("0을 나누기 때문에 종료합니다!!");
-					break;
-				}
-				number1 = divide(number1, number2);
-			}
-
-			if (count == 4)
-				break;
-			count++;
-		}
-		scan.close();
+		System.out.println("(3 + 5 - 2) X 2 / 3 - 4 = " + result);
 	}
 
 	public static float add(float x, float y) {
